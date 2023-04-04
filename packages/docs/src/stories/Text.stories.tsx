28dd6558dbe996769cefef18ee919a957578763a
Gustavo <gustavo.bernardi.ext@infracommerce.com.br>
2023-04-03 23:49:37 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,44 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    size: 'md',
+    children:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea fuga quod vel at dolorem nihil pariatur dolores quaerat dignissimos vero ipsum in culpa esse amet numquam, blanditiis praesentium odit sit.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
